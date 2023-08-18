b1a550bc103157c5e37079a876ab2f66ad1381cc
gandradep <eventsonido@gmail.com>
2023-08-18 13:20:07 +0800

    separate components: categories and category item

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import './categories.styles.scss';
+import Categories from "./components/categories/categories.component";
+
+
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats'
-    },
-    {
-      id: 1,
-      title: 'Jackets'
-    },
-    {
-      id: 1,
-      title: 'Sneakers'
-    },
-    {
-      id: 1,
-      title: 'Women'
-    },
-    {
-      id: 1,
-      title: 'Men'
-    }
-  ]
   return (
-    <div className="categories-container">
-      {categories.map(({title}) => (
-        <div className="category-container">
-        {/* </img> */}
-        <div className="category-body-container">
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      ))}
-
-    </div>
+    <Categories  />
   );
 }
 
