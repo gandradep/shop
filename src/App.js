5c902f7259fc92db41b43b16e119c21a6e840bbe
gandradep <eventsonido@gmail.com>
2023-08-21 21:36:40 +0800

    Adding react-router and some routes. Also installed firebase library

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
 
+import Navigation from './routes/navigation/navigation.component';
 import Home from "./routes/home/home.component";
+import SignIn from './routes/sign-in/sign-in.component';
 
-const Navigation = () =>{
-  return(
-    <div>
-      <div>
-        <h1>Nav Bar</h1>
-      </div>
-      <Outlet />
-      <div>
-        <h2>Footer</h2>
-      </div>
-    </div>
-  )
-
- }
+const Shop = () => (
+  <div>
+    Shop
+  </div>
+)
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         {/* index parameter is to match the path of parent element */}
         <Route index element={<Home />}/>
+        <Route path='shop' element={<Shop />}/>
+        <Route path='sign-in' element={<SignIn />}/>
       </Route>
 
     </Routes>
