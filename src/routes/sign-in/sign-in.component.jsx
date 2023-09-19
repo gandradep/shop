1ac7d4854e70ad5219e89e8a241e9b017305eafd
gandradep <eventsonido@gmail.com>
2023-09-19 17:16:58 +0800

    Creating a sign up form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import { signInWithGooglePopup, createUserDocumentFromAuthor } from '../../utils/firebase/firebase.utils'
 
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
 const SignIn = () => {
   const logGoogleUser = async () =>{
     const  {user} = await signInWithGooglePopup();
@@ -11,6 +13,7 @@ const SignIn = () => {
       <button onClick={logGoogleUser}>
         Sign in with Google
       </button>
+      <SignUpForm />
     </div>
   )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
-  signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider
 } from "firebase/auth";
